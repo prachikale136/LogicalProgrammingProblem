9312842561bd200c26ee2674ed281c41dc990a5a
Prachi <prachikale136@gmail.com>
2023-02-24 20:33:15 +0530

    [Prachi] Add: check number is prime or not

---
+++ LogicalPrograms/LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PrimeNumber
+    {
+        public static void CheckPrime()
+        {
+            Console.WriteLine("Enter the to check Prime or not");  //take input from user
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool isPrime = true;    //initialize isprime value as true
+            if (number == 1)
+            {
+                Console.WriteLine("number is neither Prime Nor Composite");
+            }
+            else if (number > 1)
+            { 
+                for(int i=2; i< number; i++) 
+                {
+                    if (number % i == 0)   // check the condition {for ex 2%2 == 0} condition true
+                    {
+                        isPrime= false;    // It means if remainder is 0 then its not prime number and break the statement  
+                        break;
+                    }
+                }
+                if(isPrime)   // check if Isprime is true then its prime 
+                {
+                    Console.WriteLine("number is prime");
+                }
+                else 
+                {
+                    Console.WriteLine("number is not prime");    //otherwise it not prime number
+                }
+            }
+
+        }
+    }
+}
