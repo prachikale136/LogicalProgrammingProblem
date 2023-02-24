6530555ee9e85e833b98b0279f891acfc2003b81
Prachi <prachikale136@gmail.com>
2023-02-24 18:57:24 +0530

    [Prachi] Add: Print the fibonacci Series

---
+++ LogicalPrograms/LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        public static int previous = 0;
+        public static int previousToPrevious = 1;
+        public static int nextTerm;
+        public static void GetSeries()
+        {
+
+            Console.WriteLine("Enter the number to series");
+            int input = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < input; i++)
+            {
+                nextTerm = previous + previousToPrevious;
+                previous = previousToPrevious;
+                previousToPrevious = nextTerm;
+
+                Console.WriteLine(nextTerm);
+            }
+
+        }
+    }
+}
