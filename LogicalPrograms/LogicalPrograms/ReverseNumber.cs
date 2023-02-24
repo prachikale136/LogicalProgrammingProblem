058883405b1d685b71c492792ee941ddbb6376da
Prachi <prachikale136@gmail.com>
2023-02-24 23:29:07 +0530

    [Prachi] Add: Reverse a number

---
+++ LogicalPrograms/LogicalPrograms/ReverseNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class ReverseNumber
+    {
+        
+        public static void IsReverse()
+        {
+            Console.WriteLine("Enter the number to reverse");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int reverse = 0;
+            while (number > 0)
+            {
+                int remainder = number % 10;
+                reverse = (reverse * 10) + remainder;
+                number = number / 10 ;
+            }
+            Console.WriteLine("Reverse no is :" +reverse);
+            
+        }
+    }
+}
